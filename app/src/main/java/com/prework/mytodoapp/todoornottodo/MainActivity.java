ab7ae1618fbeb43f2fcb8c7374e6c1dd17111553
Yoav Gray <yoavgray@gmail.com>
2016-08-01 13:36:47 +0300

    - Changed status bar color to red

--- app/src/main/java/com/prework/mytodoapp/todoornottodo/MainActivity.java
+++ app/src/main/java/com/prework/mytodoapp/todoornottodo/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.GREEN);
+            window.setStatusBarColor(Color.RED);
         }
 
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
